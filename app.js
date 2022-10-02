866fffc505d1dc7efe3a4a410f6da354ec0ef399
chrisimata21 <chrisimata21@gmail.com>
2022-10-01 19:17:32 -0700

    added html elements for file creation

--- app.js
+++ app.js
@@ -70,21 +70,122 @@ async function start() {
   const maxTimes = 4;
   for (i = 0; i < maxTimes; i++) {
     const promise = new Promise((resolve, reject) => {
-      userInput().then(function ({ name, id, email, title }) {
-        if (title === "Manager") {
-          managerInput().then(function ({ officeNumber }) {
-            this.Employee = new Manager(name, id, email, officeNumber, title);
-            employeeArr.push(Employee);
-            resolve("done");
-          });
-        } else if (title === Engineer) {
-          engineerInput().then(function ({ github }) {
-            this.employee = new Engineer(name, id, email, github, title);
-            employeeArr.push(employee);
-            resolve("done");
-          });
-        }
-      });
+      userInput()
+        .then(function ({ name, id, email, title }) {
+          if (title === "Manager") {
+            managerInput().then(function ({ officeNumber }) {
+              this.employee = new Manager(name, id, email, officeNumber, title);
+              employeeArr.push(Employee);
+              resolve("done");
+            });
+          } else if (title === "Engineer") {
+            engineerInput().then(function ({ github }) {
+              this.employee = new Engineer(name, id, email, github, title);
+              employeeArr.push(Employee);
+              resolve("done");
+            });
+          } else if (title === "Intern") {
+            internInput().then(function ({ school }) {
+              this.employee = new Intern(name, id, email, school, title);
+              employeeArr.push(Employee);
+              resolve("done");
+            });
+          }
+        })
+        .catch(function (err) {
+          console.log("There was an error.");
+          console.log(err);
+        });
     });
+    const result = await promise;
+    console.log(result);
   }
+
+  function displayTitle(employee) {
+    if (employee.title === "Manager") {
+      console.log(employee.officeNumber);
+      return `Office number: ${employee.officeNumber}`;
+    }
+    if (employee.title === "Engineer") {
+      return `School: ${employee.school}`;
+    }
+    if (employee.title === "Intern") {
+      return `GitHub: ${employee.github}`;
+    }
+  }
+  function getCardHtml() {
+    let html = "";
+    for (i = 0; i < maxTimes; i++) {
+      console.log(employeeArr[i]);
+      html += `<div class="card bg-dark justify-content-center align-items-center" style="width: 18rem;">
+        <div class="col card-header">
+            <h4>${employeeArray[j].name}</h4>
+        </div>
+        <div class="col card-header">
+            <h4>${employeeArray[j].title}</h4 >
+        </div >
+        <ul class="list-group list-group-flush text">
+            <li class="list-group-item">ID: ${employeeArray[j].id}</li>
+            <li class="list-group-item">Email: ${employeeArray[j].email}</li>
+            <li class="list-group-item"> ${displayTitle(employeeArray[j])}</li>
+        </ul>
+    </div>`;
+    }
+    return html;
+  }
+
+  let html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+    <style>
+        .row {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        }
+        .card {
+            padding: 15px;
+            border-radius: 6px;
+            background-color: white;
+            color: lightskyblue;
+            margin: 15px;
+        }
+        .text {
+            padding: 15px;
+            border-radius: 6px;
+            background-color: lightskyblue;
+            color: black;
+            margin: 15px;
+        }
+        .col {
+            flex: 1;
+            text-align: center;
+        }
+    </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark justify-content-center align-items-center">
+            <span class="navbar-brand mb-0 h1">
+            <h1>My Team</h1>
+            </span>
+        </nav>
+        <div class="row">
+            ${getCardHtml()}
+        </div>
+    </body>
+</html>`;
+  console.log(html);
+  const fs = require("fs");
+  fs.writeFile("newFile.html", html, function (err) {
+    if (err) throw err;
+    console.log("File is created successfully.");
+  });
 }
+start();
